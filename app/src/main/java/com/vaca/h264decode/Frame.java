d08c41d5ce80d78dbb84c361e71158a9448eb05a
李光浩 <liguanghao@viatomtech.com>
2021-09-14 14:27:49 +0800

    456546456

---
+++ app/src/main/java/com/vaca/h264decode/Frame.java
@@ -0,0 +1,22 @@
+package com.vaca.h264decode;
+
+/**
+ * @brief 一帧的封装
+ */
+public class Frame {
+	public byte[] mData;
+	public int offset;
+	public int length;
+
+	public Frame(byte[] data, int offset, int size) {
+		mData = data;
+		this.offset = offset;
+		this.length = size;
+	}
+
+	public void setFrame(byte[] data, int offset, int size) {
+		mData = data;
+		this.offset = offset;
+		this.length = size;
+	}
+}
