d382b5ca4914f663eaa80e6fd6ef098baa7cc6d1
李光浩 <liguanghao@viatomtech.com>
2021-09-14 14:33:36 +0800

    456546456fghg5454dscv

--- app/src/main/java/com/vaca/h264decode/MainActivity.kt
+++ app/src/main/java/com/vaca/h264decode/MainActivity.kt
@@ -2,10 +2,17 @@ package com.vaca.h264decode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vaca.h264decode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val mSurfaceView=binding.fuck
+        val mServer=Server.getInstance()
+        val mVideoDecoder=VideoDecoder(mSurfaceView.holder.surface,mServer)
+        mVideoDecoder.start()
     }
 }
